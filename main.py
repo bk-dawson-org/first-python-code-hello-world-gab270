c7d431fc153dc64522055c4826dbd4d0fd7396ba
gab270 <gabriel.donetpapp@dawsoncollege.qc.ca>
2024-08-30 23:35:17 +0000

    first work

---
+++ main.py
@@ -0,0 +1,12 @@
+print('Hello world')
+print(78-9)
+print("78-9")
+age = 20
+print("My age")
+print(age) 
+age = input("What is your age?")
+print(age)
+
+firstname = input("Enter your first name:")
+lastname = input("Enter your last name:")
+print(firstname + " " + lastname)
