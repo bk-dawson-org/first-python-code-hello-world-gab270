639275ab43b07b0da26ff8e3a8fce5357b171eb1
gab270 <gabriel.donetpapp@dawsoncollege.qc.ca>
2024-08-31 02:03:59 +0000

    perimeter

---
+++ perimeter.py
@@ -0,0 +1,11 @@
+# perimeter of a rectangle
+x = int(input("height of rectangle:"))
+y = int(input("width of rectangle:"))
+z = x + y 
+print (z)
+
+# surface of a rectangle 
+a = int(input("height of rectangle:"))
+b = int(input("width of rectangle:"))
+c = a * b 
+print (c)
